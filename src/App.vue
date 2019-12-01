f37e0fc0ef443092b18cce6c3608e5d25963af2f
llamicron <llamicron@gmail.com>
2019-11-30 21:26:45 -0600

    Adds a navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
 
+    <nav>
+      <ul class="nav-ul">
+        <router-link class="nav-item" active-class="active" to="/notes">Notes;</router-link>
+        <router-link class="nav-item" active-class="active" to="/edit">New;</router-link>
+      </ul>
+    </nav>
+
+
     <router-view/>
 
   </div>
@@ -10,9 +18,35 @@
   body {
     margin: 0;
   }
+
   #app {
     background-color: #211F30;
     height: 100vh;
     width: 100vw;
+  }
+
+  .nav-ul {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .nav-item {
+    display: inline;
+
+    color: rgb(212, 209, 236);
+
+    padding: 2em;
+    margin: 0;
+    font-family: "Fira Code";
+    font-size: 20px;
+    cursor: pointer;
+
+    transition: background-color 0.2s;
+  }
+
+  .active {
+    background-color:rgb(75, 71, 109);
   }
 </style>
